4541eca45855e0c729ce384511293929ab28bbba
Asaf Maor <asafmaor1986@gmail.com>
2015-12-03 11:14:32 +0200

    Fixed spooler

--- dummyspooler/main.py
+++ dummyspooler/main.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
 import tornado.web
+import markup
 
+jobs = []
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -8,9 +10,25 @@ class MainHandler(tornado.web.RequestHandler):
         super(MainHandler, self).initialize()
 
     def post(self):
+        global jobs
+        jobs.append(self.request.arguments['job_name'])
         self.write({'success': 1, 'message': 'Great Success'})
 
+
+class JobsHandlers(tornado.web.RequestHandler):
+    def get(self):
+        global jobs
+        page = markup.page()
+        page.init(title='Spooler Jobs')
+        page.h1('Requested jobs')
+        page.ul()
+        for job in jobs:
+            page.li(job)
+        page.ul.close()
+        self.write(str(page))
+
 if __name__ == '__main__':
-    app = tornado.web.Application([(r'^/', MainHandler)])
+    app = tornado.web.Application([(r'^/jobs$', JobsHandlers),
+                                   (r'^/.*', MainHandler)])
     app.listen(1234)
     tornado.ioloop.IOLoop.current().start()
